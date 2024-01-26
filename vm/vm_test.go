7e13f7da5517a00bab69744f5ebc85537fcffdf8
Pascal Sochacki <pascal@sochacki.dev>
2024-01-26 08:06:29 +0100

    page 174

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -15,6 +15,58 @@ type vmTestCase struct {
 	expected interface{}
 }
 
+func TestFunctionsWithReturnStatement(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+           let earlyExit = fn() { return 99; 100; };
+           earlyExit();
+           `,
+			expected: 99,
+		},
+		{
+			input: `
+           let earlyExit = fn() { return 99; return 100; };
+           earlyExit();
+           `,
+			expected: 99,
+		},
+	}
+	runVmTests(t, tests)
+}
+
+func TestCallingFunctionsWithoutArguments(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+        let fivePlusTen = fn() { 5 + 10; };
+        fivePlusTen();
+        `,
+			expected: 15,
+		},
+		{
+			input: `
+           let one = fn() { 1; };
+           let two = fn() { 2; };
+           one() + two()
+           `,
+			expected: 3,
+		},
+		{
+			input: `
+           let a = fn() { 1 };
+           let b = fn() { a() + 1 };
+           let c = fn() { b() + 1 };
+           c();
+           `,
+			expected: 3,
+		},
+	}
+
+	runVmTests(t, tests)
+
+}
+
 func TestIndexExpressions(t *testing.T) {
 	tests := []vmTestCase{
 		{"[1,2,3][1]", 2},
